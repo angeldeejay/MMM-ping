a28d3cd8978f9ddbaeeacf84f5a803acd21859f1
Andres Vanegas <ukab72106@gmail.com>
2022-08-17 00:42:05 -0500

    Parallel pings

--- node_helper.js
+++ node_helper.js
@@ -11,13 +11,13 @@
  * @external node_helper
  * @see https://github.com/MichMich/MagicMirror/blob/master/modules/node_modules/node_helper/index.js
  */
-const NodeHelper = require('node_helper');
+const NodeHelper = require("node_helper");
 
 /**
  * @external ping
  * @see https://www.npmjs.com/package/ping
  */
-const ping = require('ping');
+const ping = require("ping");
 
 /**
  * @module node_helper
@@ -39,16 +39,27 @@ module.exports = NodeHelper.create({
      * @returns {void}
      */
     async socketNotificationReceived(notification, payload) {
-        if (notification === 'CHECK_HOSTS') {
-            const status = [];
+        if (notification === "CHECK_HOSTS") {
+            const orderedStatuses = [];
+            const statuses = [];
+
+            // Ensure to make all ping requests in parallel
+            await Promise.all(
+                payload.map(async (h) => {
+                    const { alive } = await ping.promise.probe(h.host, {
+                        timeout: 1,
+                    });
+
+                    statuses.push({ online: alive, ...h });
+                })
+            );
 
             for (const h of payload) {
-                const { alive } = await ping.promise.probe(h.host);
-
-                status.push({ online: alive, ...h });
+                const s = statuses.find((s) => s.host == h.host);
+                orderedStatuses.append(s);
             }
 
-            this.sendSocketNotification('STATUS_UPDATE', status);
+            this.sendSocketNotification("STATUS_UPDATE", orderedStatuses);
         }
     },
 });
